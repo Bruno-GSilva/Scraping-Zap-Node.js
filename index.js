bd60b7f2349250177365fcb509476af1dfa4d2cf
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-09-30 21:33:54 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  
+  const totalPages = 3;
+
+  const allPropertyData = [];
+
+  for (let num_page = 1; num_page <= totalPages; num_page++) {
+    await page.goto(
+      `https://www.zapimoveis.com.br/aluguel/terrenos-lotes-comerciais/ce+fortaleza/?__ab=exp-aa-test:B,new-discover-zap:alert,vas-officialstore-social:enabled&transacao=aluguel&onde=,Cear%C3%A1,Fortaleza,,,,,city,BR%3ECeara%3ENULL%3EFortaleza,-3.73272,-38.527013,&tipos=lote-terreno_comercial&pagina=${num_page}`
+    );
+
+    const list_card = await page.evaluate(() => {
+      const nodeList = document.querySelectorAll(".result-card");
+      const cardList = [...nodeList];
+
+      const propertyData = cardList.map((item) => {
+        const propertyInfo = {};
+
+        const locationElement = item.querySelector(".card__address");
+        propertyInfo["Localização"] = locationElement
+          ? locationElement.textContent.trim()
+          : "";
+
+        const addressElement = item.querySelector(".card__street");
+        propertyInfo["Endereço"] = addressElement
+          ? addressElement.textContent.trim()
+          : "";
+
+        const descriptionElement = item.querySelector(".card__description");
+        propertyInfo["Descrição"] = descriptionElement
+          ? descriptionElement.textContent.trim()
+          : "";
+
+        const contactElement = item.querySelector(
+          ".result-card__cta-buttons button:last-child"
+        );
+        propertyInfo["Contato"] = contactElement
+          ? contactElement.textContent.trim()
+          : "";
+
+        const sizeElement = item.querySelector(".card__amenities .card__amenity");
+        propertyInfo["Tamanho"] = sizeElement
+          ? sizeElement.textContent.trim()
+          : "";
+
+        const priceTotalElement = item.querySelector(".listing-price .l-text");
+        propertyInfo["Preço Total"] = priceTotalElement
+          ? priceTotalElement.textContent.trim()
+          : "";
+
+        const priceAluguelElement = item.querySelector(
+          ".listing-price .l-text:last-child"
+        );
+        propertyInfo["Aluguel"] = priceAluguelElement
+          ? priceAluguelElement.textContent.trim()
+          : "";
+
+        const highlightsElement = item.querySelector(
+          ".l-card__tag--top .l-tag-card__content"
+        );
+        propertyInfo["Destaques"] = highlightsElement
+          ? highlightsElement.textContent.trim()
+          : "";
+
+        return propertyInfo;
+      });
+
+      return propertyData;
+    });
+
+    allPropertyData.push(...list_card);
+    
+    await page.waitForTimeout(3000);
+  }
+
+  fs.writeFileSync("card.json", JSON.stringify(allPropertyData, null, 2), (err) => {
+    if (err) throw new Error("error");
+    console.log("done!");
+  });
+
+  await browser.close();
+})();
