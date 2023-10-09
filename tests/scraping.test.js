b14d20442ece6e440d6165c330b9d3cc09706c74
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-10-09 09:40:44 -0300

    Feat: config jest

---
+++ tests/scraping.test.js
@@ -0,0 +1,28 @@
+const { sum } = require("./scraping");
+
+// describe("math funcs", () => {
+//   //beforeAll
+//   beforeAll(() => {
+//     console.log("Inicialize");
+//   });
+//   //beforeAllFunc
+//   beforeEach(() => {
+//     console.log("clearAll()");
+//   });
+//   // tests
+//   it("adds 1 + 2 to equal 3", () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+
+//   it("adds 1 + 2 to equal 5", () => {
+//     expect(sum(1, 4)).toBe(5);
+//   });
+//   // after all
+//   afterAll(() => {
+//     console.log("acabou");
+//   });
+//   //afterAllFunc
+//   afterEach(() => {
+//     console.log("clearAll()");
+//   });
+// });
