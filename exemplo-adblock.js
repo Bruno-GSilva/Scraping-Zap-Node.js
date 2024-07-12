8b4bdf960de4094d84b8918cc2b3f92ec65d904f
Bruno Silva <brunogomesdsilva8@gmail.com>
2024-07-12 08:05:33 -0300

    Create exemplo-adblock.js

---
+++ exemplo-adblock.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer-extra');
+
+// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+// Add adblocker plugin to block all ads and trackers (saves bandwidth)
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
