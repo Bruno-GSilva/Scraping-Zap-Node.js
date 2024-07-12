33aaac5feab3b05513de58e259774eb707244476
Bruno Silva <brunogomesdsilva8@gmail.com>
2024-07-12 08:09:15 -0300

    Update and rename exemplo-adblock.js to exemplo-puppeteer

--- exemplo-adblock.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer-extra');
-
-// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
-
-// Add adblocker plugin to block all ads and trackers (saves bandwidth)
-const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
